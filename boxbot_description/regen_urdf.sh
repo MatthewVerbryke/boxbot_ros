15ba51a13a11597666ec5bd2f9bd6d31e8083fad
Matthew Verbryke <verbrymr@mail.uc.edu>
2022-09-16 13:45:51 -0400

    Update URDF regen script for noetic

--- boxbot_description/regen_urdf.sh
+++ boxbot_description/regen_urdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2018-2020 University of Cincinnati
+# Copyright 2018-2022 University of Cincinnati
 # All rights reserved. See LICENSE file at:
 # https://github.com/MatthewVerbryke/boxbot_ros
 # Additional copyright may be held by others, as reflected in the commit history.
@@ -12,9 +12,7 @@ ABSOLUTE_PATH="`( cd \"$RELATIVE_PATH\" && pwd )`"
 cd $ABSOLUTE_PATH/robots/
 rm boxbot_6dof.urdf
 rm boxbot_5dof.urdf
-#rm boxbot_test_block.urdf
 
 #Create new URDF files
-rosrun xacro xacro.py -o boxbot_6dof.urdf boxbot_6dof.urdf.xacro
-rosrun xacro xacro.py -o boxbot_5dof.urdf boxbot_5dof.urdf.xacro
-#rosrun xacro xacro.py -o boxbot_test_block.urdf boxbot_test_block.urdf.xacro
+xacro boxbot_6dof.urdf.xacro > boxbot_6dof.urdf
+xacro boxbot_5dof.urdf.xacro > boxbot_5dof.urdf
